e4ef2058cf5e2aaacf51dd7a45fc26f927489fed
syuo7 <syuo7@naver.com>
2020-04-23 16:08:06 +0900

    Modify a variables.tf

--- variables.tf
+++ variables.tf
@@ -16,3 +16,44 @@ variable "public_subnets" {
   type = list(string)
   default = []
 }
+
+variable "private_subnets" {
+    description = "A list of private subnets inside the VPC"
+    type = list(String)
+    default = []
+}
+
+variable "database_subnets" {
+    description "A list of database subnets"
+    type = list(string)
+    default = []
+}
+
+variable "azs" {
+    description = "A list of availability zones names or ids in the region"
+    type = list(string)
+    default = []
+}
+
+variable "enable_dns_hostnames" {
+    description "Should be true to enable DNS hostnames in the VPC"
+    type = bool
+    default = false
+}
+
+variable "enable_dns_support" {
+    description = "Should be true to enable DNS support in the VPC"
+    type = bool
+    default = true
+}
+
+
+variable "tags" {
+    description = "A map of tags to add to all resources"
+    type = "map"
+    default = {}
+}
+
+  
+
+
