9f8d1d40795c05fb1a118320e79e1f076543eb27
syuo7 <syuo7@naver.com>
2020-04-26 18:06:10 +0900

    add gitignore, main, variables.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@
 ######
 
 resource "aws_vpc" "this" {
-    cidr_block = var.cidr
-    enable_dns_hostnames = var.enable_dns_hostname
-    enable_dns_support = var.enable_dns_support
-    tags = merge(
-        {
-            "Name" = format("%s",var.name)
-        },
-        var.tags
-    )
+  cidr_block           = var.cidr
+  enable_dns_hostnames = var.enable_dns_hostname
+  enable_dns_support   = var.enable_dns_support
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
 }
 
 ###################
@@ -21,86 +21,259 @@ resource "aws_vpc" "this" {
 ###################
 
 resource "aws_internet_gateway" "this" {
-    vpc_id = aws_vpc.this.id
-    tags = merge(
-        {
-            "Name" = format("%s",var.name)
-        },
-        var.tags
-    )
+  vpc_id = aws_vpc.this.id
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
 }
 
 #######################
 # Default Network ACLs
 #######################
 
-resource "aws_default_network_acl" "default_acl" { 
-    default_network_acl_id = aws.vpc.this.default_network_acl_id
-    
-    ingress {
-        protocol = -1
-        rule_no = 100
-        action = "allow"
-        cidr_block = "0.0.0.0/0"
-        from_port = 0
-        to_port = 0
-    }
+resource "aws_default_network_acl" "default_acl" {
+  default_network_acl_id = aws.vpc.this.default_network_acl_id
 
-    egress {
-        protocol = -1
-        rule_no = 100
-        action = "allow"
-        cidr_block = "0.0.0.0/0"
-        from_port = 0
-        to_port = 0
-    }
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
-    subnet_ids = [
-        aws_subnet.public.*.id,
-        aws_subnet.private.*.id,
-        aws_subnet.db.*.id
-    ]
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
-    tags = merge(
-        {
-        "Name" = format("%s",var.name)
-        },
-        var.tags
-    )
+  subnet_ids = [
+    aws_subnet.public.*.id,
+    aws_subnet.private.*.id,
+    aws_subnet.db.*.id
+  ]
+
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
 }
 
 #########################
 # default security group
 #########################
-resource "aws_default_security_group" "default_sg" { 
-    vpc_id = aws_vpc.this.id
-    
-    ingress {
-        protocol = -1
-        self = true
-        from_port = 0
-        to_port = 0
-    }
+resource "aws_default_security_group" "default_sg" {
+  vpc_id = aws_vpc.this.id
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = merge(
-        {
-            "Name" = format("%s",var.name)
-        },
-        var.tags
-    )
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = merge(
+    {
+      "Name" = format("%s", var.name)
+    },
+    var.tags
+  )
 }
 ################
 # Public subnet
 ################
 
-resource "aws_subnet" "public" { 
-    count = length(var.public_subnets)
-    
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(
+    {
+      "Name" = format("%s-public-%s", var.name, var.azs[count.index])
+    },
+    var.tags
+  )
+}
+
+################
+# Private subnet
+################
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(
+    {
+      "Name" = format("%s-private-%s", var.name, var.azs[count.index])
+    },
+    var.tags
+  )
+}
+
+##########################
+# Private database subnet 
+##########################
+
+resource "aws_subnet" "database" {
+  count = length(var.database_subnets)
+
+  vpc_id            = aws_var.this.id
+  cidr_block        = var.database_subnet[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(
+    {
+      "Name" = format("%s-db-%s", var.name, var.azs[count.index])
+    },
+    var.tags
+  )
+}
+
+resource "aws_db_subnet_group" "database" {
+  count = length(var.database_subnet) > 0 ? 1 : 0
+
+  name = lower(var.name)
+  description = "Database subnet group for ${var.name}"
+  subnet_ids = aws_subnet.database.*.id
   
+  tags = merge(
+      {
+          "Name" = format("%s", var.name)
+      },
+      var.tags
+  )
+}
+
+######################
+# EIP for NAT Gateway
+######################
+
+resource "aws_eip" "nat" {
+  count = length(var.azs)
+  vpc = true
+
+  tags = merge (
+      {
+      "Name" = format("%s-%s", var.name, var.azs[count.index])
+      },
+      var.tags
+  )
+}
+##############
+# NAT Gateway
+##############
+
+resource "aws_nat_gateway" "this" {
+  count = length(var.azs)
+
+  allocation_id = aws_eip.nat.*.id[count.index]
+  subnet_id = aws_subnet.public.*.id[count.index]
+}
+
+#####################
+# Public route table
+#####################
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route { 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws.internet_gateway.this.id    
+  }
+
+  tags = merge(
+      {
+          "Name" = format("%s-public", var.name)
+      },
+      var.tags
+  )
+}
+
+#####################
+# Public route table
+#####################
+
+resource "aws_route_table" "private" {
+  count = length(var.azs)
+  vpc_id = aws_vpc.this.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.*.id[count.index]
+  }
+
+  tags = merge(
+    {
+      "Name" = format("%s-private-%s", var.name, var.azs[count.index])
+    },
+    var.tags
+  )
+}
+
+######################
+# Private route table
+######################
+
+resource "aws_route_table" "private" {
+  count = length(var.azs)
+  vpc_id = aws_vpc.this.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.*.id[count.index]
+  }
+
+  tags = merge(
+    {
+      "Name" = format("%s-private-%s", var.name, var.azs[count.index])
+    },
+    var.tags
+  )
+}
+
+##########################
+# route table association
+##########################
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+
+  subnet_id = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.public.*.id[count.index]
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+
+  subnet_id = aws_subnet.private.*.id[count.index]
+  route_table_id = aws_route_table.private.*.id[count.index]
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnets)
+
+  subnet_id = aws_subnet.database.*.id[count.index]
+  route_table_id = aws_route_table.private.*.id[count.index]
 }
--- variables.tf
+++ variables.tf
@@ -2,58 +2,56 @@
 
 variable "name" {
   description = "prefix for any resource name define on the module"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "cidr" {
   description = "The CIDR block for VPC"
-  type = string
+  type        = string
 }
 
 variable "public_subnets" {
   description = "A list of public subnets inside the VPC"
-  type = list(string)
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "private_subnets" {
-    description = "A list of private subnets inside the VPC"
-    type = list(string)
-    default = []
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
 }
 
 variable "database_subnets" {
-    description = "A list of database subnets"
-    type = list(string)
-    default = []
+  description = "A list of database subnets"
+  type        = list(string)
+  default     = []
 }
 
 variable "azs" {
-    description = "A list of availability zones names or ids in the region"
-    type = list(string)
-    default = []
+  description = "A list of availability zones names or ids in the region"
+  type        = list(string)
+  default     = []
 }
 
 variable "enable_dns_hostnames" {
-    description = "Should be true to enable DNS hostnames in the VPC"
-    type = bool
-    default = true
+  description = "Should be true to enable DNS hostnames in the VPC"
+  type        = bool
+  default     = true
 }
 
 variable "enable_dns_support" {
-    description = "Should be true to enable DNS support in the VPC"
-    type = bool
-    default = true
+  description = "Should be true to enable DNS support in the VPC"
+  type        = bool
+  default     = true
 }
 
 
 variable "tags" {
-    description = "A map of tags to add to all resources"
-    type = map
-    default = {}
+  description = "A map of tags to add to all resources"
+  type        = map
+  default     = {}
 }
-
-  
 
 
